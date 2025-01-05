cc0331b14921b000d633b92f817c244b97d11909
Aditya Bhatt <142151739+AdityaBhatt37@users.noreply.github.com>
2025-01-05 17:08:28 +0000

    updating all the notes and programs of codespace in the git repo.

---
+++ db.sql
@@ -0,0 +1,26 @@
+CREATE TABLE student (
+
+    RollNo number(6),
+    Name varchar(15),
+    City varchar(12),
+    Pincode varchar(8),
+    Age number(2)
+);
+
+
+
+Insert into student(RollNo,Name,City,Pincode,Age)values(1,'Amit','Delhi','214213',21);
+Insert into student(RollNo,Name,City,Pincode,Age)values(2,'Kumar','Bombay',NULL,25);
+Insert into student(RollNo,Name,City,Pincode,Age)values(3,'Kshitij','Madras','110101',16);
+Insert into student(RollNo,Name,City,Pincode,Age)values(4,'Punnet','calcutta','110107',18);
+Insert into student(RollNo,Name,City,Pincode,Age)values(5,'Rohit','Delhi','502207',19);
+Insert into student(RollNo,Name,City,Pincode,Age)values(6,'Karnal','Dehradun',NULL,28);
+Insert into student(RollNo,Name,City,Pincode,Age)values(7,'Shweta','Dehradun',98102,23);
+Insert into student(RollNo,Name,City,Pincode,Age)values(8,'Mohit','Bombay',NULL,17);
+Insert into student(RollNo,Name,City,Pincode,Age)values(9,'Pankaj','Bombay',NULL,17);
+Insert into student(RollNo,Name,City,Pincode,Age)values(10,'Parul','dELhi','76312',24);
+Insert into student(RollNo,Name,City,Pincode,Age)values(11,'Rohit','Baroda','12131',2);
+
+
+
+
